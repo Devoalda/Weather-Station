516514006dc561bf0681b8f031037bd42c0b10e8
xypoon <xiangyuanpoon@gmail.com>
2023-03-16 20:19:42 +0800

    Updated Display, fixed nav

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,10 +1,12 @@
 import datetime
+import pickle
 from flask import Flask, render_template, request, url_for, redirect
 import json
 import requests
 from socket import *
 from pprint import pprint
 import ssl
+import re
 
 # https://ipinfo.io/json
 app = Flask(__name__)
@@ -16,36 +18,37 @@ def search():
     def get_weather_from_Server(country):
         # Server Config
         # Change IP to your server IP
-        serverIP = "127.0.0.2"
-        serverPort = 12000
+        try:
+            serverIP = "127.0.0.1"
+            serverPort = 12000
 
-        context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        context.load_verify_locations('../backend/SSL/certificate.pem')
-        context.check_hostname = False
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+            context.load_verify_locations('../backend/SSL/certificate.pem')
+            context.check_hostname = False
 
-        clientSocket = context.wrap_socket(socket(AF_INET, SOCK_STREAM), server_hostname=serverIP)
-        clientSocket.connect((serverIP, serverPort))
+            clientSocket = context.wrap_socket(socket(AF_INET, SOCK_STREAM), server_hostname=serverIP)
+            clientSocket.connect((serverIP, serverPort))
 
-        clientSocket.send(country.encode())
-        buffer = 20480
-        data = clientSocket.recv(buffer).decode()
-        if data == "Error":
-            payload = None
-        else:
-            payload = json.JSONDecoder().decode(data)
-        clientSocket.close()
-        # print(payload)
-        return payload
+            clientSocket.send(country.encode())
+            buffer = 20480
+            data = clientSocket.recv(buffer).decode()
+            if data == "Error":
+                payload = None
+            else:
+                payload = json.JSONDecoder().decode(data)
+            clientSocket.close()
+            # print(payload)
+            return payload
+        except:
+            return None
 
     if request.method == 'POST':
         location = request.form['location']
         resp = get_weather_from_Server(location)
-        print(resp)
         if resp is None:
             return redirect('404')
         if resp:
             return redirect('location/' + location)
-
     return render_template('search.html')
 
 
@@ -54,32 +57,43 @@ def index(country):  # put application's code here
     def get_weather_from_Server(country):
         # Server Config
         # Change IP to your server IP
-        serverIP = "127.0.0.2"
-        serverPort = 12000
+        try:
+            serverIP = "127.0.0.1"
+            serverPort = 12000
 
-        context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        context.load_verify_locations('../backend/SSL/certificate.pem')
-        context.check_hostname = False
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+            context.load_verify_locations('../backend/SSL/certificate.pem')
+            context.check_hostname = False
 
-        clientSocket = context.wrap_socket(socket(AF_INET, SOCK_STREAM), server_hostname=serverIP)
-        clientSocket.connect((serverIP, serverPort))
+            clientSocket = context.wrap_socket(socket(AF_INET, SOCK_STREAM), server_hostname=serverIP)
+            clientSocket.connect((serverIP, serverPort))
 
-        clientSocket.send(country.encode())
-        buffer = 20480
-        data = clientSocket.recv(buffer).decode()
-        if data == "Error":
-            payload = None
-        else:
-            payload = json.JSONDecoder().decode(data)
-        clientSocket.close()
-        # print(payload)
-        return payload
+            clientSocket.send(country.encode())
+            buffer = 20480
+            data = clientSocket.recv(buffer).decode()
+            if data == "Error":
+                payload = None
+            else:
+                payload = json.JSONDecoder().decode(data)
+            clientSocket.close()
+            # print(payload)
+            return payload
+        except:
+            return None
+
 
     apiData = get_weather_from_Server(country)
+    if apiData is None:
+        return redirect('404')
+
 
     def countryRegionDetails(data):
         res = list(apiData.keys())[0].split(",")
         newList = [res[0].lstrip(), res[1].lstrip()]
+        print("country:",newList[0])
+        print("region:",newList[1])
+        if newList[0] == newList[1]:
+            print(True)
         return newList
 
     def getCurrentCondition(apiData):
@@ -113,11 +127,14 @@ def index(country):  # put application's code here
     print(country)
     return render_template("index.html", date=getDate(), humidity=getHumidity(apiData),
                            countryRegion=countryRegionDetails(apiData),
-                           hourlyData=getHourData(apiData), currentCondition=getCurrentCondition(apiData), location=location)
+                           hourlyData=getHourData(apiData), currentCondition=getCurrentCondition(apiData),
+                           location=location)
+
 
 @app.route('/404')
 def error404():
     return render_template('404.html'), 404
+
 
 @app.errorhandler(404)
 def page_not_found(e):
